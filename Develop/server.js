a67b1c81f34b1c2d21523d383ba094fb53e917dc
Clayton S <desertcow07@gmail.com>
2022-06-20 20:11:41 -0700

    Added DB connection setup, connected to DB, building out menus

---
+++ Develop/server.js
@@ -0,0 +1,74 @@
+//
+//
+// npm i uuid
+//
+
+//! ================= Import Section =================
+
+
+
+const express = require('express');
+const mysql = require('mysql2');
+const cTable = require('console.table');
+
+
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
+//! ================= DB Connection =================
+
+const PORT = process.env.PORT || 3001;
+const HOST = '192.168.25.10';
+const HOSTPORT = 3306;
+const DATABASENAME = 'CMSEmployee';
+
+// Connect to remote mySQL instance
+const db = mysql.createConnection(
+  {
+    host: HOST,
+    port: HOSTPORT,
+    user: 'root',
+    password: 'm!m2022Tiger',
+    database: DATABASENAME,
+  },
+  console.log(`\n` + `Connected to the \x1b[42m${DATABASENAME}\x1b[0m database hosted on \x1b[47m` + HOST + ":" + HOSTPORT + `\x1b[0m\n`)
+);
+
+//! ======================= Routing =====================
+
+
+
+
+//! ================= Functions =================
+
+function DBTest() {
+
+  console.table([
+    {
+      name: 'foo',
+      age: 10
+    }, {
+      name: 'bar',
+      age: 20
+    }
+  ]);
+
+};
+
+
+//! ================= Main  =================
+
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+
+//! ================= EOF  =================
